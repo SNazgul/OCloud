80709b82975b191c85db0dcbd82f40ebe9bd83a3
SNazgul <s.nazgul@gmail.com>
2018-07-11 21:18:19 +0300

    Starting authentification

---
+++ OCloud/Controllers/AuthController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Text;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Http;
+using System.Net;
+using Microsoft.Extensions.Logging;
+
+namespace OCloud.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AuthController : Controller
+    {
+        private IConfiguration _config;
+        private readonly ILogger<AuthController> _logger;
+
+
+        public AuthController(IConfiguration config, ILogger<AuthController> logger)
+        {
+            _config = config;
+            _logger = logger;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        [ActionName("login")]
+        public IActionResult CreateToken([FromBody]LoginModel login)
+        {
+            IActionResult response = Unauthorized();
+            var user = Authenticate(login);
+
+            if (user != null)
+            {
+                var tokenString = BuildToken(user);
+                response = Ok(new { token = tokenString });
+            }
+
+            return response;
+        }
+
+        private string BuildToken(UserModel user)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+              _config["Jwt:Issuer"],
+              expires: DateTime.Now.AddMinutes(30),
+              signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private UserModel Authenticate(LoginModel login)
+        {
+            UserModel user = null;
+
+            if (login.Username == "mario" && login.Password == "secret")
+            {
+                user = new UserModel { Name = "Mario Rossi", Email = "mario.rossi@domain.com" };
+            }
+            return user;
+        }
+
+        //public static bool IsLocal(this HttpRequest req)
+        //{
+        //    var connection = req.HttpContext.Connection;
+        //    if (connection.RemoteIpAddress. .IsSet())
+        //    {
+        //        //We have a remote address set up
+        //        return connection.LocalIpAddress. .IsSet()
+        //            //Is local is same as remote, then we are local
+        //            ? connection.RemoteIpAddress.Equals(connection.LocalIpAddress)
+        //            //else we are remote if the remote IP address is not a loopback address
+        //            : IPAddress.IsLoopback(connection.RemoteIpAddress);
+        //    }
+
+        //    return true;
+        //}
+        [AllowAnonymous]
+        [HttpPost]
+        [ActionName("register")]
+        public IActionResult Register(LoginModel user)
+        {
+            _logger.LogTrace($"Enter: {nameof(Register)}");
+
+            _logger.LogDebug($"CreateUser HttpContext.Connection.RemoteIpAddress = {HttpContext.Connection.RemoteIpAddress}");
+            if (HttpContext.Connection.RemoteIpAddress != null)
+                return new UnauthorizedResult();
+
+
+            return Ok();
+        }
+
+
+        //[AllowAnonymous]
+        //[HttpGet]
+        //[ActionName("register")]
+        //public IActionResult Register()
+        //{
+        //    _logger.LogDebug($"D:CreateUser for ");
+        //    _logger.LogTrace($"T:CreateUser for ");
+        //    _logger.LogWarning($"W:CreateUser for ");
+
+        //    _logger.LogDebug($"CreateUser HttpContext.Connection = {HttpContext.Connection}");
+        //    if (HttpContext.Connection.RemoteIpAddress != null)
+        //        return new UnauthorizedResult();
+
+        //    return Ok();
+        //}
+
+        public class LoginModel
+        {
+            public string Username { get; set; }
+            public string Password { get; set; }
+            public override string ToString()
+            {
+                return $"Username: {Username}, pwd: {Password?.Length}";
+            }
+        }
+
+        private class UserModel
+        {
+            public string Name { get; set; }
+            public string Email { get; set; }
+            public DateTime Birthdate { get; set; }
+        }
+    }
+}
---
+++ Tests/OCloud.Tests/AssemblyInitializer.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.PlatformAbstractions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using NLog.Web;
+
+namespace OCloud.Tests
+{
+    [TestClass]
+    public class AssemblyInitializer
+    {
+        static TestServer _testServer;
+        static HttpClient _httpClient;
+
+
+        public static HttpClient Client => _httpClient;
+
+        [AssemblyInitialize()]
+        public static void AssemblyInit(TestContext context)
+        {
+            var rootPath = GetContentRootPath();
+            var builder = new WebHostBuilder()
+                    .UseContentRoot(rootPath)
+                    .UseEnvironment("Development")
+                    .UseConfiguration(new ConfigurationBuilder()
+                        .SetBasePath(rootPath)
+                        .AddJsonFile("appsettings.json")
+                        .AddJsonFile("appsettings.Development.json")
+                        .Build()
+                        )
+                    .UseStartup<Startup>() // Uses Start up class from your API Host project to configure the test server
+                    .ConfigureLogging(logging =>
+                    {
+                        logging.ClearProviders();
+                        logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+                    })
+                    .UseNLog();  // NLog: setup NLog for Dependency injection;  
+
+            _testServer = new TestServer(builder);
+            _httpClient = _testServer.CreateClient();
+        }
+
+        [AssemblyCleanup()]
+        public static void AssemblyCleanup()
+        {
+            Client.Dispose();
+            _testServer.Dispose();
+        }
+        
+
+        static private string GetContentRootPath()
+        {
+            var testProjectPath = PlatformServices.Default.Application.ApplicationBasePath;
+            var relativePathToHostProject = @"..\..\..\..\..\OCloud\";
+            return Path.Combine(testProjectPath, relativePathToHostProject);
+        }        
+    }
+}
