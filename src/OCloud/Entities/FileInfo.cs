f86c655be95410a2a29b4c1f7926d62bb169c634
SNazgul <s.nazgul@gmail.com>
2018-09-09 00:38:27 +0300

    Adding the model

---
+++ src/OCloud/Entities/FileInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OCloud.Entities
+{
+    public class FileInfo
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid UID { get; set; }
+
+        public string Path { get; set; }
+
+        public string FileName { get; set; }
+
+        [ForeignKey("CloudInfo")]
+        public Guid CloudId { get; set; }
+    }
+}
