c1d4cbc2ac285cbeb8e90acae637fe706d38e9e2
SNazgul <s.nazgul@gmail.com>
2018-11-26 00:14:53 +0200

    Allowing anonymous ping

--- src/OCloud/Controllers/PingController.cs
+++ src/OCloud/Controllers/PingController.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace OCloud.Controllers
 {
     [Route("api/[controller]")]
+    [AllowAnonymous]
     public class PingController : Controller
     {
         // GET api/ping
